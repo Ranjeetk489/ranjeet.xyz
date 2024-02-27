6c7fca5ec1dce8df613b0b5c3dcf0c3b21556547
Ranjeet Kumar <ranjeet.kumar@leverageedu.com>
2024-02-27 23:41:29 +0530

    init setip

--- src/app/projects/hangman-game/layout.tsx
+++ src/app/projects/hangman-game/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`bg-hero-pattern`}>{children}</body>
-    </html>
+    <div className="bg-parent">
+    <div className={``}>{children}</div>
+    </div>
   );
 }
--- src/app/projects/hangman-game/page.tsx
+++ src/app/projects/hangman-game/page.tsx
@@ -4,17 +4,18 @@ import Image from "next/image"
 const Page = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-[321px] h-[532px] relative">
-      {/* <div>
+      <div className="w-[321px] h-[532px] relative bg-blue">
+        <div>
           <h4>The</h4>
           <h1>Hangman</h1>
           <h4>Game</h4>
         </div>
         <div className="flex justify-center items-center">
-            <div className="w-[200px] h-[200] rounded-full relative">
-                <Image src={`${IMAGE_DOMAIN}/assets/hangman.png`} alt="Hangman" layout="fill" objectFit="cover" />
-            </div>
-        </div> */}
+          <div className="w-[200px] h-[200px] rounded-full relative bg-pink-400 shadow-pink-bs">
+            <Image src={`${IMAGE_DOMAIN}/assets/play.svg`} alt="Hangman" height={40} width={40} className="absolute top-1/2 left-1/2" />
+          </div>
+          <button>How to play</button>
+        </div>
       </div>
     </div>
   )
