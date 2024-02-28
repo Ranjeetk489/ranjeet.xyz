44545f879995a48ce1f2aa7983cb5550f7f0c688
Ranjeet Kumar <ranjeet.kumar@leverageedu.com>
2024-02-28 23:06:43 +0530

    added linter

--- src/app/projects/hangman-game/category/page.tsx
+++ src/app/projects/hangman-game/category/page.tsx
@@ -24,16 +24,18 @@ interface Data {
 
 const Page = async () => {
   const host = headers().get("host");
-  const protocal = process?.env.NODE_ENV==="development"?"http":"https"
-  let response = await fetch(`${protocal}://${host}/api`, { cache: "no-store" });
+  const protocal = process?.env.NODE_ENV === "development" ? "http" : "https";
+  let response = await fetch(`${protocal}://${host}/api`, {
+    cache: "no-store",
+  });
   const data: Data = await response.json();
 
   return (
     <div className="">
-      <div className="bg-gradient-to-b from-[#1A043A] to-[#2B1677] fixed opacity-75 h-screen w-screen -z-50" />
-      <div className="p-6 flex flex-col gap-6 container mx-auto">
+      <div className="fixed -z-50 h-screen w-screen bg-gradient-to-b from-[#1A043A] to-[#2B1677] opacity-75" />
+      <div className="container mx-auto flex flex-col gap-6 p-6">
         <BackButton title="Pick A Category" />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-[16px] md:gap-[32px] lg:gap-x-[32px] lg:gap-y-[50px] mt-14 md:mt-[100px] lg:mt-[150px]">
+        <div className="mt-14 grid grid-cols-1 gap-y-[16px] md:mt-[100px] md:grid-cols-2 md:gap-[32px] lg:mt-[150px] lg:grid-cols-3 lg:gap-x-[32px] lg:gap-y-[50px]">
           {Object.entries(data.categories).map(([category]) => (
             <button
               // className="py-[24px] lg:py-[60px] md:py-[105px]"
@@ -45,14 +47,17 @@ const Page = async () => {
               key={category}
             >
               <div
-                className="bg-[#2463FF]   text-white rounded-[20px] flex justify-center py-[24px]  md:py-[60px] lg:py-[60px]"
+                className="flex   justify-center rounded-[20px] bg-[#2463FF] py-[24px] text-white  md:py-[60px] lg:py-[60px]"
                 style={{ boxShadow: `inset 0px 6px 0px #3C74FF` }}
               >
-                <div className="tracking-wider text-hm uppercase">{category}</div>
+                <div className="text-hm uppercase tracking-wider">
+                  {category}
+                </div>
               </div>
             </button>
           ))}
-          Ba</div>
+          Ba
+        </div>
       </div>
     </div>
   );
--- src/app/projects/hangman-game/howToPlay/page.tsx
+++ src/app/projects/hangman-game/howToPlay/page.tsx
@@ -1,48 +1,58 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import Back from "../../../../../public/assets/images/icon-back.svg";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 import BackButton from "@/components/projects/hangman/backButton";
 
-
 const Page = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className="">
       {/* <div className="bg-gradient-to-b from-[#1A043A] to-[#2B1677] fixed opacity-75 h-screen w-screen z-0" /> */}
-      <div className="bg-gradient-to-b from-[#1A043A] to-[#2B1677] fixed opacity-75 h-screen w-screen -z-50" />
+      <div className="fixed -z-50 h-screen w-screen bg-gradient-to-b from-[#1A043A] to-[#2B1677] opacity-75" />
       <div className="container mx-auto">
-        <BackButton title={"How To Play"}/>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-8 mt-[60px] space-y-8 items-stretch lg:space-y-0">
-          <div className="bg-white px-6 py-8 md:px-12 md:py-[60px] lg:px-10  rounded-3xl tracking-wider lg:flex lg:flex-col lg:gap-8 desc-card">
-            <p className="text-blue text-2xl md:text-hl text-center md:col-start-1 md:col-end-1 md:row-start-1 md:row-end-3 md:self-center justify-self-start lg:mb-10">01</p>
-            <p className="text-2xl text-start text-dark-navy uppercase lg:text-hm lg:text-center">Choose a category</p>
-            <p className="text-[#887DC0] text-base md:text-b leading-[150%] lg:mt-6 lg:text-center row-start-2 row-end-3 col-start-1 col-end-3 md:col-start-2 md:col-end-3 mt-2">
-              First, choose a word category, like animals or movies. The computer
-              then randomly selects a secret word from that topic and shows you
-              blanks for each letter of the word.
+        <BackButton title={"How To Play"} />
+        <div className="mt-[60px] grid grid-cols-1 items-stretch gap-x-8 space-y-8 lg:grid-cols-3 lg:space-y-0">
+          <div className="desc-card rounded-3xl bg-white px-6 py-8 tracking-wider  md:px-12 md:py-[60px] lg:flex lg:flex-col lg:gap-8 lg:px-10">
+            <p className="justify-self-start text-center text-2xl text-blue md:col-start-1 md:col-end-1 md:row-start-1 md:row-end-3 md:self-center md:text-hl lg:mb-10">
+              01
+            </p>
+            <p className="text-start text-2xl uppercase text-dark-navy lg:text-center lg:text-hm">
+              Choose a category
+            </p>
+            <p className="col-start-1 col-end-3 row-start-2 row-end-3 mt-2 text-base leading-[150%] text-[#887DC0] md:col-start-2 md:col-end-3 md:text-b lg:mt-6 lg:text-center">
+              First, choose a word category, like animals or movies. The
+              computer then randomly selects a secret word from that topic and
+              shows you blanks for each letter of the word.
             </p>
           </div>
-          <div className="bg-white px-6 py-8 md:px-12 md:py-[60px] lg:px-10  rounded-3xl tracking-wider lg:flex lg:flex-col lg:gap-8 desc-card">
-            <p className="text-blue text-2xl md:text-hl text-center md:col-start-1 md:col-end-1 md:row-start-1 md:row-end-3 md:self-center justify-self-start lg:mb-10">02</p>
-            <p className="text-2xl text-start text-dark-navy uppercase lg:text-hm lg:text-center">Guess the letters</p>
-            <p className="text-[#887DC0] text-base md:text-b leading-[150%] lg:mt-6 lg:text-center row-start-2 row-end-3 col-start-1 col-end-3 md:col-start-2 md:col-end-3 mt-2">
+          <div className="desc-card rounded-3xl bg-white px-6 py-8 tracking-wider  md:px-12 md:py-[60px] lg:flex lg:flex-col lg:gap-8 lg:px-10">
+            <p className="justify-self-start text-center text-2xl text-blue md:col-start-1 md:col-end-1 md:row-start-1 md:row-end-3 md:self-center md:text-hl lg:mb-10">
+              02
+            </p>
+            <p className="text-start text-2xl uppercase text-dark-navy lg:text-center lg:text-hm">
+              Guess the letters
+            </p>
+            <p className="col-start-1 col-end-3 row-start-2 row-end-3 mt-2 text-base leading-[150%] text-[#887DC0] md:col-start-2 md:col-end-3 md:text-b lg:mt-6 lg:text-center">
               Take turns guessing letters. The computer fills in the relevant
-              blank spaces if your guess is correct. If it`s wrong, you lose some
-              health, which empties after eight incorrect guesses.
+              blank spaces if your guess is correct. If it`s wrong, you lose
+              some health, which empties after eight incorrect guesses.
             </p>
           </div>
-          <div className="bg-white px-6 py-8 md:px-12 md:py-[60px] lg:px-10  rounded-3xl tracking-wider lg:flex lg:flex-col lg:gap-8 desc-card">
-            <p className="text-blue text-2xl md:text-hl text-center md:col-start-1 md:col-end-1 md:row-start-1 md:row-end-3 md:self-center justify-self-start lg:mb-10">01</p>
-            <p className="text-2xl text-start text-dark-navy uppercase lg:text-hm lg:text-center">WIN OR LOSE</p>
-            <p className="text-[#887DC0] text-base md:text-b leading-[150%] lg:mt-6 lg:text-center row-start-2 row-end-3 col-start-1 col-end-3 md:col-start-2 md:col-end-3 mt-2">
-            You win by guessing all the letters in the word before your health
+          <div className="desc-card rounded-3xl bg-white px-6 py-8 tracking-wider  md:px-12 md:py-[60px] lg:flex lg:flex-col lg:gap-8 lg:px-10">
+            <p className="justify-self-start text-center text-2xl text-blue md:col-start-1 md:col-end-1 md:row-start-1 md:row-end-3 md:self-center md:text-hl lg:mb-10">
+              01
+            </p>
+            <p className="text-start text-2xl uppercase text-dark-navy lg:text-center lg:text-hm">
+              WIN OR LOSE
+            </p>
+            <p className="col-start-1 col-end-3 row-start-2 row-end-3 mt-2 text-base leading-[150%] text-[#887DC0] md:col-start-2 md:col-end-3 md:text-b lg:mt-6 lg:text-center">
+              You win by guessing all the letters in the word before your health
               runs out. If the health bar empties before you guess the word, you
               lose.
             </p>
           </div>
-
         </div>
       </div>
     </div>
